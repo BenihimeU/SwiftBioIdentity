d14b08ccace51604ef246c744f85040348d19d1c
JasonGrace <Vishwa.vijay143@gmail.com>
2018-01-11 23:51:47 +0530

    Icon and Pod files added

---
+++ Pods/Target Support Files/Pods-SwiftBio/Pods-SwiftBio-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftBio : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftBio
+@end
---
+++ Pods/Target Support Files/Pods-SwiftBioUITests/Pods-SwiftBioUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftBioUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftBioUITests
+@end
